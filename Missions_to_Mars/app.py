257911bcf366595778f3dd4cf12781512654c1a2
Leif96 <lynn.leifker@gmail.com>
2019-10-24 23:31:10 -0500

    website update

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, jsonify, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import scrape
 
+# Create an instance of Flask
 app = Flask(__name__)
 
+# Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars")
 
-@app.route('/')
+# Route to index page to render Mongo data to template
+@app.route("/")
 def index():
-    mars_data = mongo.db.collection.find_one()
+
+    # Find one record of data from Mongo
+    Mars_data = mongo.db.Mars.find_one()
     
     # Activate jinja within the website index page
-    return render_template('index.html', Mars=mars_data)
+    return render_template("index.html", mars=Mars_data)
     
 
-@app.route('/scrape')
+@app.route("/scrape")
 def scrape():
 
     # Run the scrape function for Mars news and facts
-    Mars_news = scrape_mars.JPL_scrape()
+    mars_data = scrape_mars.Mars_scrape()
 
-    # mars.append(Mars_news)
-    mongo.db.Mars.update({}, Mars_news, upsert = True)
+    # mars.append
+    mongo.db.Mars.update({}, mars_data, upsert = True)
 
     # Redirect back to home page
-    return redirect('/')
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
